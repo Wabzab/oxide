99aa9c7f8e593216f98da1bddd1ec479d16e12b3
Wabz <patersonjones.max@gmail.com>
2026-05-29 09:08:41 +0200

    update: smooth camera follow

--- src/main.rs
+++ src/main.rs
@@ -3,66 +3,38 @@ use bevy::prelude::*;
 #[derive(Component)]
 struct Player;
 
-#[derive(Component)]
-struct Tile;
-
-#[derive(Component)]
-struct Chunk {
-    size: f32,
-    pub tiles: Vec<Entity>,
-}
+const PLAYER_SPEED: f32 = 100.0;
+const CAMERA_DECAY_RATE: f32 = 5.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, (move_player, check_chunks))
+        .add_systems(Update, (move_player, move_camera))
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     // Spawn the cameraman
     commands.spawn(Camera2d);
 
     // Spawn the fool
     commands.spawn((
         Player,
-        Text2d::new("@"),
-        TextFont {
-            font_size: 12.0,
-            font: default(),
-            ..default()
-        },
-        TextColor(Color::WHITE),
+        Mesh2d(meshes.add(Circle::new(10.0))),
+        MeshMaterial2d(materials.add(Color::hsl(350.0, 0.75, 0.75))),
         Transform::from_translation(Vec3::ZERO),
     ));
-
-    // Spawn the map's main chunks
-    let width: i32 = 32;
-    let height: i32 = 32;
-    let size: f32 = 256.0;
-
-    for x in 0..width {
-        for y in 0..height {
-            commands.spawn((
-                Chunk {
-                    size,
-                    tiles: vec![],
-                },
-                Transform::from_translation(Vec3 {
-                    x: (x as f32 - width as f32 / 2.0) * size,
-                    y: (y as f32 - height as f32 / 2.0) * size,
-                    z: 0.0, // Terrain layer?
-                }),
-            ));
-        }
-    }
 }
 
 fn move_player(
     input: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
-    mut player_transform: Single<&mut Transform, With<Player>>,
+    mut transform: Single<&mut Transform, With<Player>>,
 ) {
     let mut direction = Vec2::ZERO;
     if input.pressed(KeyCode::KeyA) {
@@ -79,53 +51,21 @@ fn move_player(
     }
 
     if direction != Vec2::ZERO {
-        let speed = 150.0;
-        let velocity = direction.normalize() * speed * time.delta_secs();
-        player_transform.translation.x += velocity.x;
-        player_transform.translation.y += velocity.y;
+        let velocity = direction.normalize() * PLAYER_SPEED * time.delta_secs();
+        transform.translation.x += velocity.x;
+        transform.translation.y += velocity.y;
     }
 }
 
-fn check_chunks(
-    mut commands: Commands,                       // for deleting/adding chunks
-    mut meshes: ResMut<Assets<Mesh>>,             // for tile meshes
-    mut materials: ResMut<Assets<ColorMaterial>>, // for tile colors
-    mut query: Query<(&mut Chunk, &Transform)>,
-    player_transform: Single<&Transform, With<Player>>,
+fn move_camera(
+    mut camera: Single<&mut Transform, (With<Camera2d>, Without<Player>)>,
+    player: Single<&mut Transform, (With<Player>, Without<Camera2d>)>,
+    time: Res<Time>,
 ) {
-    let render_distance = 512.0;
-    for (mut chunk, transform) in query.iter_mut() {
-        let tile_size = 32.0;
-        let tile_count = chunk.size / tile_size;
-        let distance = (transform.translation - player_transform.translation).length();
-        if chunk.tiles.len() == 0 && distance <= render_distance {
-            for x in 0..tile_count as i32 {
-                for y in 0..tile_count as i32 {
-                    let tile = commands
-                        .spawn((
-                            Tile,
-                            Transform::from_translation(
-                                transform.translation
-                                    + Vec3 {
-                                        x: (x as f32 - tile_count / 2.0) * tile_size,
-                                        y: (y as f32 - tile_count / 2.0) * tile_size,
-                                        z: 0.0,
-                                    },
-                            ),
-                            Mesh2d(meshes.add(Rectangle::new(tile_size, tile_size))),
-                            MeshMaterial2d(materials.add(Color::hsl(25.0, 0.35, 0.5))),
-                        ))
-                        .id();
-                    chunk.tiles.push(tile);
-                }
-            }
-        }
+    let Vec3 { x, y, .. } = player.translation;
+    let direction = Vec3::new(x, y, camera.translation.z);
 
-        if chunk.tiles.len() != 0 && distance > render_distance {
-            while chunk.tiles.len() > 0 {
-                let tile = chunk.tiles.pop();
-                commands.entity(tile.unwrap()).despawn();
-            }
-        }
-    }
+    camera
+        .translation
+        .smooth_nudge(&direction, CAMERA_DECAY_RATE, time.delta_secs());
 }
