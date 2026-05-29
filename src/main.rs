d57428cb49321b2d87140a0bd3d08e941f3a7513
Wabz <patersonjones.max@gmail.com>
2026-05-29 21:58:37 +0200

    update: simple procedural terrain generation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use bevy::prelude::*;
+use noise::utils::{NoiseMapBuilder, PlaneMapBuilder};
+use noise::{Fbm, Perlin};
 
 #[derive(Component)]
 struct Player;
+
+#[derive(Resource)]
+struct GameWorld {
+    noise_generator: Fbm<Perlin>,
+}
 
 const PLAYER_SPEED: f32 = 100.0;
 const CAMERA_DECAY_RATE: f32 = 5.0;
@@ -9,6 +16,9 @@ const CAMERA_DECAY_RATE: f32 = 5.0;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(GameWorld {
+            noise_generator: Fbm::<Perlin>::new(0),
+        })
         .add_systems(Startup, setup)
         .add_systems(Update, (move_player, move_camera))
         .run();
@@ -18,7 +28,30 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    world: Res<GameWorld>,
 ) {
+    // Draw procedural map
+    let noise_map = PlaneMapBuilder::<_, 3>::new(&world.noise_generator).build();
+    let (grid_width, grid_height) = noise_map.size();
+    let tile_size = 8_f32;
+    let start_x = -(grid_width as f32) * tile_size / 2.0;
+    let start_y = -(grid_height as f32) * tile_size / 2.0;
+    for col_x in 0..grid_width {
+        for col_y in 0..grid_height {
+            let val = noise_map.get_value(col_x, col_y);
+            let x = start_x + col_x as f32 * tile_size;
+            let y = start_y + col_y as f32 * tile_size;
+            commands.spawn((
+                Sprite {
+                    custom_size: Some(Vec2::new(tile_size, tile_size)),
+                    color: Color::hsl((360.0 * val) as f32, 0.5, 0.5),
+                    ..default()
+                },
+                Transform::from_translation(Vec3::new(x, y, 0.0)),
+            ));
+        }
+    }
+
     // Spawn the cameraman
     commands.spawn(Camera2d);
 
@@ -31,6 +64,7 @@ fn setup(
     ));
 }
 
+// Moves the player based on input
 fn move_player(
     input: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
@@ -57,9 +91,10 @@ fn move_player(
     }
 }
 
+// Moves the camera by smoothly following the player
 fn move_camera(
     mut camera: Single<&mut Transform, (With<Camera2d>, Without<Player>)>,
-    player: Single<&mut Transform, (With<Player>, Without<Camera2d>)>,
+    player: Single<&Transform, (With<Player>, Without<Camera2d>)>,
     time: Res<Time>,
 ) {
     let Vec3 { x, y, .. } = player.translation;
